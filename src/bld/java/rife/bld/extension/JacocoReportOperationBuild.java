068b0b8be5ebdce4f94bcd62d69e4d0581d74dab
Erik C. Thauvin <erik@thauvin.net>
2024-07-03 15:31:41 -0700

    Version 0.9.6

--- src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
+++ src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
@@ -34,7 +34,7 @@ public class JacocoReportOperationBuild extends Project {
     public JacocoReportOperationBuild() {
         pkg = "rife.bld.extension";
         name = "JacocoReportOperation";
-        version = version(0, 9, 6, "SNAPSHOT");
+        version = version(0, 9, 6);
 
         javaRelease = 17;
 
