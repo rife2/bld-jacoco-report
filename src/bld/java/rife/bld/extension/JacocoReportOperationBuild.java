57af36e761416e43ee85b4bcbe8f9a3c56546aed
Erik C. Thauvin <erik@thauvin.net>
2024-07-22 17:47:06 -0700

    Bumped bld to version 2.0.0-SNAPSHOT

--- src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
+++ src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
@@ -25,8 +25,7 @@ import rife.bld.publish.PublishScm;
 
 import java.util.List;
 
-import static rife.bld.dependencies.Repository.MAVEN_CENTRAL;
-import static rife.bld.dependencies.Repository.RIFE2_RELEASES;
+import static rife.bld.dependencies.Repository.*;
 import static rife.bld.dependencies.Scope.*;
 import static rife.bld.operations.JavadocOptions.DocLinkOption.NO_MISSING;
 
@@ -34,22 +33,22 @@ public class JacocoReportOperationBuild extends Project {
     public JacocoReportOperationBuild() {
         pkg = "rife.bld.extension";
         name = "JacocoReportOperation";
-        version = version(0, 9, 6);
+        version = version(0, 9, 7, "SNAPSHOT");
 
         javaRelease = 17;
 
         downloadSources = true;
         autoDownloadPurge = true;
-        repositories = List.of(MAVEN_CENTRAL, RIFE2_RELEASES);
+        repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL, RIFE2_RELEASES, RIFE2_SNAPSHOTS);
 
         var jacocoVersion = new VersionNumber(0, 8, 12);
         scope(compile)
                 .include(dependency("org.jacoco", "jacoco", jacocoVersion).exclude("*", "org.jacoco.doc"))
-                .include(dependency("com.uwyn.rife2", "bld", version(1, 9, 1)));
+                .include(dependency("com.uwyn.rife2", "bld", version(2, 0, 0, "SNAPSHOT")));
         scope(test)
                 .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 3)))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 3)))
-                .include(dependency("org.assertj", "assertj-core", version(3, 26, 0)));
+                .include(dependency("org.assertj", "assertj-core", version(3, 26, 3)));
 
         javadocOperation()
                 .javadocOptions()
