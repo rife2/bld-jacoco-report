06a6777e7a4157ac999558d99715ada68c0d86dc
Erik C. Thauvin <erik@thauvin.net>
2024-08-28 10:41:55 -0700

    Bumped JUnit to version 5.11.0

--- src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
+++ src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
@@ -47,8 +47,8 @@ public class JacocoReportOperationBuild extends Project {
                 .include(dependency("org.jacoco", "jacoco", jacocoVersion).exclude("*", "org.jacoco.doc"))
                 .include(dependency("com.uwyn.rife2", "bld", version(2, 0, 1)));
         scope(test)
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 10, 3)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 10, 3)))
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 11, 0)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 11, 0)))
                 .include(dependency("org.assertj", "assertj-core", version(3, 26, 3)));
 
         javadocOperation()
