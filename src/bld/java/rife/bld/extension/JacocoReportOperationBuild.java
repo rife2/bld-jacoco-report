4224d95c56d8a9c40f7577fb304f7d8d613811f7
Erik C. Thauvin <erik@thauvin.net>
2025-08-15 17:48:34 -0700

    Use $NPM_PACKAGES to locate xunit-viewer

--- src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
+++ src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
@@ -129,16 +129,19 @@ public class JacocoReportOperationBuild extends Project {
             ex = e;
         }
 
-        var xunitViewer = new File("/usr/bin/xunit-viewer");
-        if (xunitViewer.exists() && xunitViewer.canExecute()) {
-            var reportsDir = "build/reports/tests/test/";
+        var npmPackagesEnv = System.getenv("NPM_PACKAGES");
+        if (npmPackagesEnv != null) {
+            var xunitViewer = Path.of(npmPackagesEnv, "bin", "xunit-viewer").toFile();
+            if (xunitViewer.exists() && xunitViewer.canExecute()) {
+                var reportsDir = "build/reports/tests/test/";
 
-            Files.createDirectories(Path.of(reportsDir));
+                Files.createDirectories(Path.of(reportsDir));
 
-            new ExecOperation()
-                    .fromProject(this)
-                    .command(xunitViewer.getPath(), "-r", testResultsDir, "-o", reportsDir + "index.html")
-                    .execute();
+                new ExecOperation()
+                        .fromProject(this)
+                        .command(xunitViewer.getPath(), "-r", testResultsDir, "-o", reportsDir + "index.html")
+                        .execute();
+            }
         }
 
         if (ex != null) {
