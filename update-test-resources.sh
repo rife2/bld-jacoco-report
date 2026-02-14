ec95f4c599be6a42bfeb11909bb9e93b89546f11
Erik C. Thauvin <erik@thauvin.net>
2026-02-13 22:15:42 -0800

    Update test resources

--- update-test-resources.sh
+++ update-test-resources.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 rm -rf src/test/resources/*
-examples/bld co jacoco
+examples/bld cl co jacoco
 cp -f "examples/build/main/com/example/Examples.class" src/test/resources/
 cp -f "examples/build/jacoco/jacoco.exec" src/test/resources/
