401193b96b1ac60b24d83d8de2d96b93951fdc7e
Erik C. Thauvin <erik@thauvin.net>
2026-06-02 09:19:49 -0700

    Add validation tests

--- src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
+++ src/bld/java/rife/bld/extension/JacocoReportOperationBuild.java
@@ -61,7 +61,7 @@ public class JacocoReportOperationBuild extends Project {
                         version(4, 9, 8)));
         scope(test)
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
-                        version(1, 0, 1)))
+                        version(1, 1, 0, "SNAPSHOT")))
                 .include(dependency("org.junit.jupiter", "junit-jupiter", junit))
                 .include(dependency("org.junit.platform", "junit-platform-console-standalone", junit))
                 .include(dependency("org.assertj", "assertj-core",
